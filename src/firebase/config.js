58877ccdb78eafb5a675ad0b1a5db3e6659a2ee8
SACHIN T P <sachintp1209@gmail.com>
2025-11-25 16:50:06 +0530

    updated config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -27,13 +27,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0ki1NKRZnPGvJMgyPAJIcRAb6nFRUnI8",
-  authDomain: "quizcard-7b96d.firebaseapp.com",
-  projectId: "quizcard-7b96d",
-  storageBucket: "quizcard-7b96d.firebasestorage.app",
-  messagingSenderId: "276993698684",
-  appId: "1:276993698684:web:52fdba2c5290341b5092f9",
-  measurementId: "G-Q96DKJPPQP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
